807a9d8004fd38797875ec59434083e4597fc0b4
YICSSON <yibsondavid@gmail.com>
2021-08-08 00:02:35 -0500

    listo

--- src/app/components/capitulos/capitulos.component.ts
+++ src/app/components/capitulos/capitulos.component.ts
@@ -1,47 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {PersonajesService} from '../../services/personajes/personajes.service'
-import {Capitulos} from '../../Models/capitulos'
+import { PersonajesService } from '../../services/personajes/personajes.service'
+import { Capitulos } from '../../Models/capitulos'
 @Component({
   selector: 'app-capitulos',
   templateUrl: './capitulos.component.html',
   styleUrls: ['./capitulos.component.css']
 })
 export class CapitulosComponent implements OnInit {
-
-  capitulosseleccion = '';
-
-  capitulos:any
-  array2=[]
+  capitulos: any
+  array2 = []
   //capitulos2=[]
   capitulosfiltro = ""
-  constructor(private personajesservice:PersonajesService) { }
+  constructor(private personajesservice: PersonajesService) { }
 
   ngOnInit(): void {
     this.Cargarcapitulos();
-    console.log(this.capitulosseleccion)
   }
 
-  change_url(url:string){
-    console.log(url)
-  }
 
   //cargar capitulos
-  Cargarcapitulos(){
-  try {
-    let array:any
-    this.personajesservice.CargarCapitulos().subscribe(res=>{
-    array = res
-     let capitulos2: Capitulos[]  = array.results
-     this.capitulos = capitulos2
-    })
-   
-  } catch (error) {
-    console.error('so se pudieron obtener datos')
-  }
-   
+  Cargarcapitulos() {
+    try {
+      let array: any
+      this.personajesservice.CargarCapitulos().subscribe(res => {
+        array = res
+        let capitulos2: Capitulos[] = array.results
+        this.capitulos = capitulos2
+      })
+
+    } catch (error) {
+      console.error('so se pudieron obtener datos')
+    }
+
   }
 
-  capturarPersonaje(event:any){
-    console.log(event)
-  }
 }
